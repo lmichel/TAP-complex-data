b805a8583c280f871f25c5a38a9717104142acb1
haoyun liao <liao@cds-stg-1.astro.unistra.fr>
2019-09-19 09:28:25 +0200

    modofier error

--- python/test/simbad.py
+++ python/test/simbad.py
@@ -3,5 +3,5 @@ from objintap.explorer import Explorer
 if __name__ == "__main__":
     explorer = Explorer("https://simbad.u-strasbg.fr/simbad/sim-tap", "public", "basic")
     explorer.build_hierarchy() #create all table and save the key
-    print(explorer.limit_tables(["basic", "otypes", "otypedef", "flux","filter"]))
+    print(explorer.limit_tables(["basic", "otypes", "otypedef", "flux","filter","ids", "ref", "author"]))
     #print(explorer.to_json_schema())
