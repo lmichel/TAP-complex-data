05a9b202084a0c931b6eb923768210753a636ed9
cobrecht <galyfray@gmail.com>
2021-09-15 10:18:14 +0200

    added setTableConstraints

--- api/tap/JsonAdqlBuilder.js
+++ api/tap/JsonAdqlBuilder.js
@@ -96,6 +96,46 @@ var JsonAdqlBuilder = (function(){
             table = this.adqlJsonMap.joints[table].parentNode
         }
 
+        return {"status" : true};
+
+    }
+
+    /**
+     * This method builds and set multiple constraints for a single table.
+     * @param {String} table unqualified name of the constraint table
+     * @param {Array[String]} constraints an array of valid adql constraint only constraining table, without any starting or leading adql keyword (WHERE,OR,AND, ...)
+     * @param {Array[String]} logic Optional an array of valid adql binary boolean operator (AND,OR) to be use to merge the constraints. If logic is too small and strict is either undefined or false no error is raised and the array gets looped to fill the missing blank.
+     * @param {Boolean} strict  Optional define if logic should exactly have the correct size according to logic's size
+     * @returns 
+     */
+    JsonAdqlBuilder.prototype.setTableConstraints = function(table,constraints,logic,strict){
+        if (strict){
+            if (logic.length !== constraints.length -1){
+                return {"status" : false, "error":{"logs":"logic length doesn't match with constraints length","params" : {"table":table,"constraints":constraints,"logic":logic,"strict":strict}}};
+            }
+        } 
+        
+        if (constraints == undefined){
+            return {"status" : false, "error":{"logs":"Invalid parameter : constraints","params" : {"table":table,"constraints":constraints,"logic":logic,"strict":strict}}};
+        }
+
+        if (logic == undefined){
+            logic = ["AND"];
+        }
+
+        let mainConstraint = constraints[0];
+
+        for (let i=1; i< constraints.length;i++){
+            mainConstraint += " " + logic[i%logic.length].trim() + " " + constraints[i];
+        }
+
+        let val = this.setTableConstraint(table, mainConstraint);
+        
+        if(val.status){
+            return {"status" : true, "constraint" :mainConstraint};
+        } else {
+            return {"status" : false, "error":{"logs":"Error while setting constraint :\n" + val.error.logs,"params" : {"table":table,"constraints":constraints,"logic":logic,"strict":strict}}};
+        }
     }
 
     return JsonAdqlBuilder;
