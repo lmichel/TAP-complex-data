1ac7b29412406730e8083c93f5f9d02ce5a9c589
cobrecht <galyfray@gmail.com>
2021-09-20 11:19:36 +0200

    refactored api test code

---
+++ demo/js/shared.js
@@ -0,0 +1,119 @@
+"use strict";
+
+/*/ Utility function used by all demo pages /*/
+
+function display(data, id) {
+    if (typeof data != "string"){
+        data = JSON.stringify(data,undefined,4);
+    }
+    $("#" + id).html(data);
+}
+
+function addClass(elemId,className){
+    document.getElementById(elemId).classList.add(className);
+}
+
+function removeClass(elemId,className){
+    if (document.getElementById(elemId).classList.contains(className)) {
+        document.getElementById(elemId).classList.remove(className);
+    }
+}
+
+/*/
+ * Disable : btn-dark
+ * Success : btn-success
+ * Enable  : btn-primary
+ * Errored : btn-danger
+/*/
+
+function disableButton(btnId){
+    removeClass(btnId,"btn-primary");
+    removeClass(btnId,"btn-success");
+    addClass(btnId,"btn-dark");
+}
+
+function successButton(btnId){
+    removeClass(btnId,"btn-primary");
+    removeClass(btnId,"btn-dark");
+    addClass(btnId,"btn-success");
+}
+
+function enableButton(btnId){
+    removeClass(btnId,"btn-dark");
+    removeClass(btnId,"btn-success");
+    addClass(btnId,"btn-primary");
+}
+
+function errorButton(btnId){
+    removeClass(btnId,"btn-primary");
+    removeClass(btnId,"btn-dark");
+    removeClass(btnId,"btn-success");
+    addClass(btnId,"btn-danger");
+}
+
+function isEnable(btnId){
+    return document.getElementById(btnId).classList.contains("btn-primary");
+}
+
+function isDisable(btnId){
+    return document.getElementById(btnId).classList.contains("btn-dark");
+}
+
+function isSuccess(btnId){
+    return document.getElementById(btnId).classList.contains("btn-success");
+}
+
+function bindClickEvent(elemId,handler,disableText){
+    $("#" + elemId).click(() =>{
+        try{
+
+            if (!isDisable(elemId)){
+                if(handler()){
+                    successButton(elemId);
+                }
+            } else if (disableText === undefined){
+                display("This button is currently disabled, you can't use it.", "getStatus");
+            } else {
+                display(disableText, "getStatus");
+            }
+
+        }catch(error){
+            console.error(error)
+            errorButton(elemId)
+            display("An unexpected Error has append see logs for more information", "getStatus");
+        }
+    })
+}
+
+async function syncIt(promise){
+    $("#overlay").fadeIn(1);
+    try {
+        await promise()
+    } catch (error) {
+        console.error(error);
+    }
+    $("#overlay").fadeOut(1000)
+}
+
+
+function bindClickAsyncEvent(elemId,handler,disableText){
+    $("#" + elemId).click(async() =>{ syncIt( async ()=>{
+        try{
+            if (!isDisable(elemId)){
+                let val = await handler();
+                if(val){
+                    successButton(elemId);
+                }
+            } else if (disableText === undefined){
+                display("This button is currently disabled, you can't use it.", "getStatus");
+            } else {
+                display(disableText, "getStatus");
+            }
+
+        }catch(error){
+            console.error(error)
+            errorButton(elemId)
+            display("An unexpected Error has append see logs for more information", "getStatus");
+        }
+    })});
+}
