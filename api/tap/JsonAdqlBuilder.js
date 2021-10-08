8db81ac0a4467f2b0b539f0e1501974e779941c5
galyfray <37240229+galyfray@users.noreply.github.com>
2021-10-08 16:29:23 +0200

    Develop (#27)
     - added "meta.id;src" to select name columns in KT
     - internal updates to handle single table schemes
     - implemented api getTableConstraint method
     - fixed 4XMM url
     - fixed not being able to add constraint on the root table

--- api/tap/JsonAdqlBuilder.js
+++ api/tap/JsonAdqlBuilder.js
@@ -14,58 +14,61 @@ var JsonAdqlBuilder = (function(){
 
         /*/ Building adqlJsonMap from objectMap /*/
 
-        let map = {};
-        map[this.adqlJsonMap.rootTable] = objectMap.map[objectMap.root_table.name].join_tables;
-        let nextMap;
-        let mapHasKey = true;
-        while (mapHasKey){
-            nextMap = {};
-            mapHasKey = false;
-            for (let parent in map){
-                mapHasKey = true;
-                for (let key in map[parent]){
+        if(objectMap.map[objectMap.root_table.name] !== undefined){
+            let map = {};
+            map[this.adqlJsonMap.rootTable] = objectMap.map[objectMap.root_table.name].join_tables;
+            let nextMap;
+            let mapHasKey = true;
+            while (mapHasKey){
+                nextMap = {};
+                mapHasKey = false;
+                for (let parent in map){
+                    mapHasKey = true;
+                    for (let key in map[parent]){
 
-                    /* We cant register a table twice 
-                    * so we choose to only keep the smallest path to the rooTable
-                    * we also don't want to register branches in nodeTreeBranches that their joints isn't 
-                    * in the joint map.
-                    */
-                    if(this.adqlJsonMap.joints[key] === undefined){
+                        /* We cant register a table twice 
+                        * so we choose to only keep the smallest path to the rooTable
+                        * we also don't want to register branches in nodeTreeBranches that their joints isn't 
+                        * in the joint map.
+                        */
+                        if(this.adqlJsonMap.joints[key] === undefined){
 
-                        /*/ nodeTreeBranches building /*/
-                        if (parent !== this.adqlJsonMap.rootTable){
-                            if (this.adqlJsonMap.nodeTreeBranches[parent] === undefined){
-                                this.adqlJsonMap.nodeTreeBranches[parent] = [key];
-                            }else{
-                                this.adqlJsonMap.nodeTreeBranches[parent].push(key);
+                            /*/ nodeTreeBranches building /*/
+                            if (parent !== this.adqlJsonMap.rootTable){
+                                if (this.adqlJsonMap.nodeTreeBranches[parent] === undefined){
+                                    this.adqlJsonMap.nodeTreeBranches[parent] = [key];
+                                }else{
+                                    this.adqlJsonMap.nodeTreeBranches[parent].push(key);
+                                }
+                            }
+
+                            /*/ joints map building /*/
+                            this.adqlJsonMap.joints[key] = {
+                                "from":map[parent][key].from,
+                                "target":map[parent][key].target,
+                                "parentNode" : parent
+                            };
+                        } else {
+                            console.warn("the table " + key + " is in more than one tree !");
+                        }
+                        
+                        /*/ nextMap building /*/
+                        if (map[parent][key].join_tables !== undefined){
+                            if (nextMap[key] === undefined){
+                                nextMap[key] = map[parent][key].join_tables;
+                            }else {
+                                for (let subKey in map[parent][key].join_tables){
+                                    nextMap[key][subKey] = map[parent][key].join_tables[subKey];
+                                }
                             }
                         }
 
-                        /*/ joints map building /*/
-                        this.adqlJsonMap.joints[key] = {
-                            "from":map[parent][key].from,
-                            "target":map[parent][key].target,
-                            "parentNode" : parent
-                        };
-                    } else {
-                        console.warn("the table " + key + " is in more than one tree !");
                     }
-                    
-                    /*/ nextMap building /*/
-                    if (map[parent][key].join_tables !== undefined){
-                        if (nextMap[key] === undefined){
-                            nextMap[key] = map[parent][key].join_tables;
-                        }else {
-                            for (let subKey in map[parent][key].join_tables){
-                                nextMap[key][subKey] = map[parent][key].join_tables[subKey];
-                            }
-                        }
-                    }
-
                 }
+                map = nextMap;
             }
-            map = nextMap;
         }
+        
 
     }
 
@@ -75,7 +78,7 @@ var JsonAdqlBuilder = (function(){
      */
     JsonAdqlBuilder.prototype.setTableConstraint = function(table,constraint){
 
-        if(this.adqlJsonMap.joints[table] === undefined){
+        if(this.adqlJsonMap.joints[table] === undefined && table !== this.adqlJsonMap.rootTable){
             return {"status" : false, "error":{"logs":"Unknown table " + table,"params" : {"table":table,"constraint":constraint}}};
         }
 
